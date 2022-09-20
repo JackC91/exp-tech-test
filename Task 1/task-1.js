924e0d8aee21759432213c3674c0381551b67892
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-20 18:48:44 +0100

    simple solution coded, works when K param does not go over the days array length

--- Task 1/task-1.js
+++ Task 1/task-1.js
@@ -1,1 +1,18 @@
-console.log("testing")
+/*
+Days of the week are represented as three-letter strings ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+
+Write a function (called solution) that, given a string S representing the day of the week and an integer K (between 0 and 500), returns the day of the week that is K days later
+
+Ie 
+If S = "Wed" and K = 2 the function should return "Fri"
+Or if S = "Sat" and K = 23 the function should return "Mon"
+*/
+
+function solution(S, K) {
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+let day_index = days.indexOf(S)
+let final_day = day_index + K
+return days[final_day]
+}
+
+console.log(solution("Wed", 5));
