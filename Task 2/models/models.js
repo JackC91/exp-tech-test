66a8f78900b4f6fd4ce94f88e5b05ac7df712f41
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-21 11:24:06 +0100

    console logs removed and empty array returned if name does not exist

--- Task 2/models/models.js
+++ Task 2/models/models.js
@@ -3,7 +3,6 @@ import data from "../db/index.js";
 export default function getUsers(user_name) {
     let searched_user = [];
     if (!user_name) {
-        // console.log("no query")
         return data;
     } else {
         for (let i = 0; i < data.length; i++) {
@@ -11,7 +10,6 @@ export default function getUsers(user_name) {
                 searched_user.push(data[i]);
             }
         }
-        // console.log("name was in query")
         return searched_user;
     }
 }
