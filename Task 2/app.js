69ef218e9ab00df20a5b1ad3595731022598aa18
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-21 09:42:53 +0100

    test route working

--- Task 2/app.js
+++ Task 2/app.js
@@ -1,1 +1,18 @@
 //main page for task 2 solve
+
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.json({
+    success: true,
+    message: "Test route up and running!",
+  });
+});
+
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
+});
