7fb610fb6e308aaf133a80a288f07081d2aa151d
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-21 09:40:22 +0100

    test data added

---
+++ Task 2/db/index.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        "id": 1,
+        "name": "John",
+        "role": "admin"
+    },
+    {
+        "id": 2,
+        "name": "Juan",
+        "role": "developer"
+    }
+]
+
+export default data;
