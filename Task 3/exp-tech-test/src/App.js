893890949d9907c47478913009d7882b13b2292e
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-21 12:07:46 +0100

    task 3 done

--- Task 3/exp-tech-test/src/App.js
+++ Task 3/exp-tech-test/src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-function UserName() {
-  const [ user, setUser ] = useState("");
+function UserName({setUser}) {
 
   function captureUser(e) {
     setUser(e.target.value);
@@ -12,30 +11,39 @@ function UserName() {
     <input id="username-input" type="text" onChange={captureUser}/>
   )}
 
-  function Password() {
-    const [ password, setPassword ] = useState("");
+  function Password({setPassword}) {
   
     function capturePassword(e) {
       setPassword(e.target.value);
-      console.log(password)
     }
   
     return (
       <input id="password-input" type="password" onChange={capturePassword}/>
     )}
 
-function SubmitButton() {
+function SubmitButton({isDisabled, user, password}) {
   return (
-    <button id="login-button">Submit</button>
+    <button id="login-button" disabled={isDisabled} onClick={() => console.log(user, password)}>Submit</button>
   )
 }
 
+
 function App() {
+  const [ user, setUser ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ isDisabled, setIsDisabled ] = useState(true)
+
+useEffect(() => {
+  if (user.length > 1 && password.length > 1) {
+    setIsDisabled(false)
+  }
+}, [user, password])
+
   return (
     <div className="App">
-    <UserName/>
-    <Password/>
-    <SubmitButton/>
+    <UserName setUser={setUser} user={user}/>
+    <Password setPassword={setPassword} password={password}/>
+    <SubmitButton isDisabled={isDisabled} user={user} password={password}/>
     </div>
   );
 }
