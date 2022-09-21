559bce6badb3017e2d880a6b435df3585309ebd1
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-21 11:45:15 +0100

    password input working, submit button added, functionality to be finished

--- Task 3/exp-tech-test/src/App.js
+++ Task 3/exp-tech-test/src/App.js
@@ -12,10 +12,30 @@ function UserName() {
     <input id="username-input" type="text" onChange={captureUser}/>
   )}
 
+  function Password() {
+    const [ password, setPassword ] = useState("");
+  
+    function capturePassword(e) {
+      setPassword(e.target.value);
+      console.log(password)
+    }
+  
+    return (
+      <input id="password-input" type="password" onChange={capturePassword}/>
+    )}
+
+function SubmitButton() {
+  return (
+    <button id="login-button">Submit</button>
+  )
+}
+
 function App() {
   return (
     <div className="App">
     <UserName/>
+    <Password/>
+    <SubmitButton/>
     </div>
   );
 }
